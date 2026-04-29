4ebba3fb243cf2f137ed5f6417186799d1313aa5
Aneela <125266894+AneelaGandepalli@users.noreply.github.com>
2026-04-29 14:30:07 +0200

    rebase fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,4 @@
 import "dotenv/config";
-<<<<<<< HEAD
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
@@ -44,14 +43,3 @@ if (
     process.exit(0);
   });
 }
-=======
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
-
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
->>>>>>> 31e25fd (Editing sign in and register)
