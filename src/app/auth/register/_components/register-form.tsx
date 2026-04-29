bc5c529648c9db08e24c7c0d776cf8850a513176
Aneela <125266894+AneelaGandepalli@users.noreply.github.com>
2026-04-29 15:27:30 +0200

    Rebase Fix2

--- src/app/auth/register/_components/register-form.tsx
+++ src/app/auth/register/_components/register-form.tsx
@@ -1,31 +1,34 @@
 "use client"
 
 import { useRouter } from "next/navigation";
-import { z} from "zod"
+import { z } from "zod"
 import { useForm } from "@tanstack/react-form";
 import { authClient } from "@/lib/auth-client";
 
 const formSchema = z.object({
-  name: z.string().min(1,"Name required").max(32),
+  name: z.string().min(1, "Name required").max(32),
   email: z.email().max(40),
   password: z.string().min(1).max(20)
 });
 
 export default function RegisterForm() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const form = useForm({
-      defaultValues: {
-        name: "",
-        email: "",
-      },
-      validators: {
-        onSubmit: formSchema,
-      },
-      onSubmit: async ({ value }) => {
-        const { error } = await authClient.signUp.email({
-
-        })
-      }
-    })
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    validators: {
+      onSubmit: formSchema,
+    },
+    onSubmit: async ({ value }) => {
+      const { error } = await authClient.signUp.email({
+        name: value.name,
+        email: value.email,
+        password: value.password,
+      })
+    }
+  })
 }
--- src/app/auth/register/sign-in/_components/sign-in-form.tsx
+++ src/app/auth/register/sign-in/_components/sign-in-form.tsx
@@ -8,32 +8,33 @@ import { authClient } from "@/lib/auth-client";
 
 
 const formSchema = z.object({
-name: z.string().min(1,"Name required").max(32),
-email: z.email().max(40),
+  email: z.email().max(128),
+  password: z.string().min(8).max(128),
 });
 
 export default function Signinform() {
- const router = useRouter();
+  const router = useRouter();
 
- const form = useForm({
-  defaultValues: {
-    email: "",
-    password: "",
-  },
-  validators: {
-    onSubmit: formSchema,
-  },
-  onSubmit: async ({ value }) => {
-    const { error } = await authClient.signIn.email({
-       email: value.email,
-       password: value.password,  
-    });
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    validators: {
+      onSubmit: formSchema,
+    },
 
-    if (error) {
-      if (error.code === "EMAIL_NOT_VERIFIED") {
-        router.push ("/auth/verify-email");
-      }    
+    onSubmit: async ({ value }) => {
+      const { error } = await authClient.signIn.email({
+        email: value.email,
+        password: value.password,
+      });
+
+      if (error) {
+        if (error.code === "EMAIL_NOT_VERIFIED") {
+          router.push("/auth/verify-email");
+        }
+      }
     }
-  }  
- })
+  })
 }
