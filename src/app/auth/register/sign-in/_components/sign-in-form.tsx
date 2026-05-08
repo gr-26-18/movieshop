7842acf5815a70c671ea71a134471a594b9483ed
Tomas3241 <tomas.shaba@lexlink.se>
2026-05-08 15:47:23 +0200

    Authentication

--- src/app/auth/register/sign-in/_components/sign-in-form.tsx
+++ src/app/auth/register/sign-in/_components/sign-in-form.tsx
@@ -1,21 +1,32 @@
-"use client"
+"use client";
 
-import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 import { useForm } from "@tanstack/react-form";
 import { useRouter } from "next/navigation";
-import { authClient } from "@/lib/auth-client";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
-
-
+import { z } from "zod";
 
 const formSchema = z.object({
   email: z.email().max(128),
   password: z.string().min(8).max(128),
 });
 
-export default function Signinform() {
+export default function SignInForm() {
   const router = useRouter();
 
   const form = useForm({
@@ -26,7 +37,6 @@ export default function Signinform() {
     validators: {
       onSubmit: formSchema,
     },
-
     onSubmit: async ({ value }) => {
       const { error } = await authClient.signIn.email({
         email: value.email,
@@ -38,8 +48,9 @@ export default function Signinform() {
           router.push("/auth/verify-email");
           return;
         }
+
         alert(error.message || "An unknown error occurred");
-        return; 
+        return;
       }
 
       router.push("/");
@@ -50,18 +61,18 @@ export default function Signinform() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Sign in</CardTitle>
+        <CardTitle>Sign In</CardTitle>
         <CardDescription>Fill out the form below to sign in</CardDescription>
       </CardHeader>
       <CardContent>
-        <form 
-             id="sign-in-form"
-             onSubmit={(ev) => {
-              ev.preventDefault();
-              form.handleSubmit(ev);
-             }}
-             >
-        <FieldGroup>
+        <form
+          id="sign-in-form"
+          onSubmit={(ev) => {
+            ev.preventDefault();
+            form.handleSubmit(ev);
+          }}
+        >
+          <FieldGroup>
             <form.Field name="email">
               {(field) => {
                 const isInvalid =
@@ -87,7 +98,41 @@ export default function Signinform() {
                 );
               }}
             </form.Field>
+
+            <form.Field name="password">
+              {(field) => {
+                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid;
+
+                return (
+                  <Field data-invalid={isInvalid}>
+                    <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                    <Input
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(ev) => field.handleChange(ev.target.value)}
+                      aria-invalid={isInvalid}
+                      autoComplete="new-password"
+                      type="password"
+                    />
+                    {isInvalid && (
+                      <FieldError errors={field.state.meta.errors} />
+                    )}
+                  </Field>
+                );
+              }}
+            </form.Field>
+          </FieldGroup>
+        </form>
       </CardContent>
+
+      <CardFooter>
+        <Button className="w-full" form="sign-in-form">
+          Sign In
+        </Button>
+      </CardFooter>
     </Card>
-  )
+  );
 }
